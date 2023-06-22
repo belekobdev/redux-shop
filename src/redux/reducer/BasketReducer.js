2b24278e881072c678158a877be1107dac6bfbc6
unknown <belekobdev>
2023-06-22 15:33:15 +1300

    init

--- src/redux/reducer/BasketReducer.js
+++ src/redux/reducer/BasketReducer.js
@@ -8,9 +8,7 @@ export const BasketReducer = (state = initialState, action) =>  {
         case "ADD_TO_BASKET" : {
             let bas = state.basket.find(el => el.id === action.payload.id)
             if (bas) {
-                return {
-                    ...state,
-                    basket: state.basket.map((el) => el.id === bas.id ? {...el, count: el.count + 1} : el)
+                return {...state, basket: state.basket.map((el) => el.id === bas.id ? {...el, count: el.count + 1} : el)
                 }
             } else {
                 return {...state, basket: [...state.basket, {...action.payload, count: 1}]}
